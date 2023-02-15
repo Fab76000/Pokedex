2702be8fcbd5174586ed0e77be4a745dc6e57b2f
Fabienne <fabienne_berges@yahoo.fr>
2023-02-15 19:59:46 +0100

    fix app.js

--- app.js
+++ app.js
@@ -4,29 +4,28 @@ const searchInput = document.querySelector('.recherche-poke input');
 const listePoke = document.querySelector('.liste-poke');
 const chargement = document.querySelector('.loader');
 
-
-
 const types = {
     grass: '#78c850',
-	ground: '#E2BF65',
-	dragon: '#6F35FC',
-	fire: '#F58271',
-	electric: '#F7D02C',
-	fairy: '#D685AD',
-	poison: '#966DA3',
-	bug: '#B3F594',
-	water: '#6390F0',
-	normal: '#D9D5D8',
-	psychic: '#F95587',
-	flying: '#A98FF3',
-	fighting: '#C25956',
+    ground: '#E2BF65',
+    dragon: '#6F35FC',
+    fire: '#F58271',
+    electric: '#F7D02C',
+    fairy: '#D685AD',
+    poison: '#966DA3',
+    bug: '#B3F594',
+    water: '#6390F0',
+    normal: '#D9D5D8',
+    psychic: '#F95587',
+    flying: '#A98FF3',
+    fighting: '#C25956',
     rock: '#B6A136',
     ghost: '#735797',
     ice: '#96D9D6'
 }
 
 
-function fetchPokemonBase(){
+
+function fetchPokemonBase() {
 
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(reponse => reponse.json())
@@ -37,7 +36,6 @@ function fetchPokemonBase(){
             })
 
         })
-
 }
 
 fetchPokemonBase();
@@ -47,50 +45,49 @@ function fetchPokemonComplet(pokemon) {
     let objPokemonFull = {};
     let url = pokemon.url;
     let nameP = pokemon.name;
+
     fetch(url)
-    .then(reponse => reponse.json())
-    .then((pokeData) => {
-         console.log(pokeData);
-
-        objPokemonFull.pic = pokeData.sprites.front_default;
-        objPokemonFull.type = pokeData.types[0].type.name;
-        objPokemonFull.id = pokeData.id;
-		
-
-
-        fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
         .then(reponse => reponse.json())
-        .then((pokeData)) => {
+        .then((pokeData) => {
             //console.log(pokeData);
 
-            objPokemonFull.name = pokeData.names[4].name;
-            allPokemon.push(objPokemonFull);
+            objPokemonFull.pic = pokeData.sprites.front_default;
+            objPokemonFull.type = pokeData.types[0].type.name;
+            objPokemonFull.id = pokeData.id;
 
-            if(allPokemon.length === 151) {
-                // console.log(allPokemon);
 
-                tableauFin = allPokemon.sort((a,b) => {
-                    return a.id - b.id;
-                }).slice(0,21);
-                 //console.log(tableauFin);
 
-                createCard(tableauFin);
-                chargement.style.display = "none";
-				
-				
-            }
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+                .then(reponse => reponse.json())
+                .then((pokeData) => {
+                    //console.log(pokeData);
+
+                    objPokemonFull.name = pokeData.names[4].name;
+                    allPokemon.push(objPokemonFull);
+
+                    if (allPokemon.length === 151) {
+                        // console.log(allPokemon);
+
+                        tableauFin = allPokemon.sort((a, b) => {
+                            return a.id - b.id;
+                        }).slice(0, 21);
+                        // console.log(tableauFin);
+
+                        createCard(tableauFin);
+                        chargement.style.display = "none";
+                    }
+
+                })
 
         })
-
-    })
 
 }
 
 // Création des cartes
 
-function createCard(arr){
+function createCard(arr) {
 
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
         const carte = document.createElement('li');
         let couleur = types[arr[i].type];
@@ -101,7 +98,6 @@ function createCard(arr){
         idCarte.innerText = `ID# ${arr[i].id}`;
         const imgCarte = document.createElement('img');
         imgCarte.src = arr[i].pic;
-		const poke_types = pokemon.types.map(el=> el.type.name);
 
         carte.appendChild(imgCarte);
         carte.appendChild(txtCarte);
@@ -111,18 +107,21 @@ function createCard(arr){
 
     }
 
+
 }
+
+
 
 // Scroll Infini
 
 window.addEventListener('scroll', () => {
 
-    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     // scrollTop = scroll depuis le top
     // scrollHeight = scroll total
     // clientHeight = hauteur de la fenêtre, partie visible.
 
-    if(clientHeight + scrollTop >= scrollHeight - 20) {
+    if (clientHeight + scrollTop >= scrollHeight - 20) {
         addPoke(6);
     }
 
@@ -131,7 +130,7 @@ window.addEventListener('scroll', () => {
 let index = 21;
 
 function addPoke(nb) {
-    if(index > 151) {
+    if (index > 151) {
         return;
     }
     const arrToAdd = allPokemon.slice(index, index + nb);
@@ -144,15 +143,15 @@ function addPoke(nb) {
 
 // const formRecherche = document.querySelector('form');
 // formRecherche.addEventListener('submit', (e) => {
-    //     e.preventDefault();
-    //     recherche();
-    // })
-    
+//     e.preventDefault();
+//     recherche();
+// })
+
 searchInput.addEventListener('keyup', recherche);
 
-function recherche(){
+function recherche() {
 
-    if(index < 151) {
+    if (index < 151) {
         addPoke(130);
     }
 
@@ -160,13 +159,13 @@ function recherche(){
     filter = searchInput.value.toUpperCase();
     allLi = document.querySelectorAll('li');
     allTitles = document.querySelectorAll('li > h5');
-    
-    
-    for(i = 0; i < allLi.length; i++) {
+
+
+    for (i = 0; i < allLi.length; i++) {
 
         titleValue = allTitles[i].innerText;
 
-        if(titleValue.toUpperCase().indexOf(filter) > -1) {
+        if (titleValue.toUpperCase().indexOf(filter) > -1) {
             allLi[i].style.display = "flex";
         } else {
             allLi[i].style.display = "none";
@@ -181,9 +180,9 @@ function recherche(){
 
 // Animation Input
 
-searchInput.addEventListener('input', function(e) {
+searchInput.addEventListener('input', function (e) {
 
-    if(e.target.value !== "") {
+    if (e.target.value !== "") {
         e.target.parentNode.classList.add('active-input');
     } else if (e.target.value === "") {
         e.target.parentNode.classList.remove('active-input');
